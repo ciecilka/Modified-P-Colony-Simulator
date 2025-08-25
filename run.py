f88da824fb6a5a785d22e106438f0a691444e902
ciecilka <57717338+ciecilka@users.noreply.github.com>
2025-08-25 13:21:57 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,10 @@
 from PCOL_lib import Agent, Colony, Rule
 import fce
-from fce import read_colony_from_file
 import math
 
 def main():
     file="ukazka.txt"
-    col1=read_colony_from_file(file)
+    col1=fce.read_colony_from_file(file)
     res=fce.computation(col1,4)
     print(res)
 
